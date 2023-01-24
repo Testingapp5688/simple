25cd17e81c2fd08080ec932164bfe51f8ab588ca
Testingapp5688 <123411932+Testingapp5688@users.noreply.github.com>
2023-01-24 06:22:38 +0530

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+
+const app=express();
+app.get('/user',(req,res)=>{
+    res.json({message:"form user"});
+});
+app.get('/',(req,res)=>{
+    res.json({message:"Done the Works"});
+});
+app.listen(4600);
